9a530733b85de9a81cd6fda844310e8c43692ff5
HongLeongYong <32185642+HongLeongYong@users.noreply.github.com>
2022-08-04 16:33:14 +0800

    Update 2.4

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var copyright = document.getElementById("Copyright_label");
+    copyright.textContent = "Copyright ©" + new Date().getFullYear();
+});
+
